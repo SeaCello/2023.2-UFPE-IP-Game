900c42fe00955ff9759c7eb5f7ef1efd46bf1e86
Gui-Rigaud <guirigaud@hotmail.com>
2024-03-02 14:05:14 -0300

    Adicionando imagens de fundo e do personagem para fins de testes, classe do personagem e corrigindo erros

--- Projeto/scripts/main.py
+++ Projeto/scripts/main.py
@@ -1,4 +1,8 @@
+import os
+os.environ['SDL_AUDIODRIVER'] = 'dsp'
+
 import pygame
+from nico import player
 
 # pygame setup
 pygame.init()
@@ -6,6 +10,10 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+fundo_img = pygame.image.load('Projeto/assets/imagem_de_fundo.jpg')
+
+person_img = pygame.image.load('Projeto/assets/test_person2.png')
+person = player()
 
 while running:
 
@@ -16,5 +24,10 @@ while running:
     pygame.display.flip()
 
     dt = clock.tick(60) / 1000
+
+    person.movimenta(dt)
+
+    screen.blit(fundo_img, (0, 0))
+    screen.blit(person_img, (person.player_posx, person.player_posy))
 
 pygame.quit()
