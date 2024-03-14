2b5d52701a927d38c2d082b20802187ec59ff89c
Marcello Eduardo Alves de Menezes <meam@WINDOWS.CIN.UFPE.BR>
2024-03-14 14:55:27 -0300

    inicio dos scripts de level design

--- Projeto/scripts/main.py
+++ Projeto/scripts/main.py
@@ -1,4 +1,8 @@
 import pygame
+import level
+from level import Platform
+
+
 
 # pygame setup
 pygame.init()
@@ -6,13 +10,20 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+blocks = pygame.sprite.Group()
+levelMap = level.levelDesign
 
 while running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    
+    for blockY in range(len(levelMap)):
+        for blockX in range(len(levelMap[blockY])):
+            if(levelMap[blockY][blockX] == 1):            
+                blocks.add(Platform(blockX*32+16, blockY*32+16))
+    blocks.draw(screen)
     pygame.display.flip()
 
     dt = clock.tick(60) / 1000
